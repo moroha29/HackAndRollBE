1e7f5a6ff02a1116cc6ee79c911fa3613958a4e1
moroha29 <84175294+moroha29@users.noreply.github.com>
2024-01-20 23:37:38 +0800

    less unhinged questions

--- src/gpt/filter.py
+++ src/gpt/filter.py
@@ -13,14 +13,18 @@ for x in messages[-301:]:
     # temp="["
     # for i in x['answers']:temp+=json.dumps(i)+","
     # temp=temp[:-1]+"]"
+    temp=x['answers'][0]['text']
     for i in range(len(x['question'])): 
         if "\""==x['question'][i]: x['question']=x['question'][:i]+"'"+x['question'][i+1:]
-    final_data+="{\"messages\": [{\"role\": \"system\", \"content\": \"PaisehBot is a generative bot that comes up with random extremely embarrassing question relating to an individual personal life and life choices.\"}, {\"role\": \"user\", \"content\": \"Give me a paiseh question\"}, {\"role\": \"assistant\", \"content\": \""+x['question']+"\"}]}\n"
+    for i in range(1,len(x['answers'])):
+        temp+=" || "+x['answers'][i]['text']
+    final_data+="{\"messages\": [{\"role\": \"system\", \"content\": \"PaisehBot is a generative bot that comes up with random extremely embarrassing question relating to an individual personal life and life choices and give options to the questions.\"}, {\"role\": \"user\", \"content\": \"Give me a paiseh question\"}, {\"role\": \"assistant\", \"content\": \""+"Question:"+x['question']+" Options:"+temp+"\"}]}\n"
 # final_data=[{"messages": [{"role": "system", "content": "PaisehBot is a generative bot that comes up with random embarrassing question."}, {"role": "user", "content": "Give me a paiseh question"}, {"role": "assistant", "content": {'question':x['question'],'answers':x['answers']}}]} for x in messages]
 
 # Transform python object back into json
 # output_json = json.dumps(final_data[-101:])
 
-with open("filtered.jsonl", "w") as outfile: outfile.write(final_data)
+with open("filtered.jsonl", "w",encoding="utf-8") as outfile: 
+    outfile.write(final_data)
 
 input_json.close()
