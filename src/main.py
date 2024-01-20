ecfde3bc576b28db1e4bc870daeead9df5152228
Jin Wei <seetohjinwei@gmail.com>
2024-01-20 14:34:29 +0800

    Add loadenv

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
 from .routers import router
+from dotenv import load_dotenv
+from fastapi import FastAPI
+import os
+
+load_dotenv()
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+
+print(MONGO_USERNAME, MONGO_PASSWORD)
+
 
 app = FastAPI()
 
