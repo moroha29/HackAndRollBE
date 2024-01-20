82efda811151f3f86b502dc61e634cd770ea3b35
Jin Wei <seetohjinwei@gmail.com>
2024-01-20 20:40:01 +0800

    Add CORS

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from .routers import router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router)
